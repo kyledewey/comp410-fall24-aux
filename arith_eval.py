87b8030819949bfb26b945b15fb382ad6337e836
Kyle Dewey <kyle.dewey@csun.edu>
2024-09-09 15:19:38 -0700

    .

--- arith_eval.py
+++ arith_eval.py
@@ -1,5 +1,42 @@
+# Java equivalent:
+#
+# public class IntegerLiteral {
+#   public int value;
+#   public IntegerLiteral(int v) {
+#     this.value = v;
+#   }
+# }
+#
 class IntegerLiteral:
     def __init__(self, v):
         self.value = v
 
-# TODO: rest of AST, and evaluator
+class Plus:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Multiply:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class UnaryMinus:
+    def __init__(self, child):
+        self.child = child
+
+# takes an AST node
+# returns whatever AST node evaluates to
+# (an integer)
+def evaluate(node):
+    if isinstance(node, IntegerLiteral):
+        return node.value
+    elif isinstance(node, Plus):
+        l = evaluate(node.left)
+        r = evaluate(node.right)
+        return l + r
+        #return evaluate(node.left) + evaluate(node.right)
+    elif isinstance(node, Multiply):
+        return evaluate(node.left) * evaluate(node.right)
+    elif isinstance(node, UnaryMinus):
+        return -evaluate(node.child)
