fb5c3ca2d3fa50163d4a7da19711e17fd11a9b2a
Kyle Dewey <kyle.dewey@csun.edu>
2024-10-21 15:45:43 -0700

    .

--- lists.pl
+++ lists.pl
@@ -12,9 +12,14 @@
 %
 myAppend([], List, List).
 myAppend([Head|Tail], List, [Head|Rest]) :-
-    % Output = [Head|Rest],
-    myAppend(Tail, List, Rest).
+    myAppend(Tail, List, Rest). % tail call - last thing done is recursive call
 
+% int sum(int[] arr) {
+%   int retval = 0;
+%   for (int index = 0; index < arr.length; index++) {
+%     retval = retval + arr[index];
+%   }
+% }
 sumAll([], 0).
 sumAll([Head|Tail], Sum) :-
     sumAll(Tail, X),
@@ -25,3 +30,29 @@ sublist([Head|Tail], [Head|X]) :-
     sublist(Tail, X).
 sublist([_|Tail], X) :-
     sublist(Tail, X).
+
+% int length(List input) {
+%   int len = 0; // accumulator
+%   while (input is not empty) {
+%     len++;
+%     input = input.tail;
+%   }
+%   return len;
+% }
+myLength([], 0).
+myLength([_|Tail], Len) :-
+    myLength(Tail, SubLen),
+    Len is 1 + SubLen.
+
+% myLengthAccum(List, LengthOfList)
+myLengthAccum(List, Len) :-
+    myLengthAccum(List, 0, Len).
+
+% myLengthAccum(List, Accumulator, LengthOfList)
+myLengthAccum([], Accum, Accum).
+myLengthAccum([_|Tail], Accum, Len) :-
+    NewAccum is Accum + 1,
+    myLengthAccum(Tail, NewAccum, Len).
+
+% Tail-call optimization / tail recursion optimization
+% -O(n) stack space usage -> O(1)
